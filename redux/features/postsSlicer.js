cecab2ffd375c0e60dd3b2634f5c61b314e15e40
sherif.butt@gmail.com <sherif.butt@gmail.com>
2022-05-12 12:40:58 +0100

    added additional text to post content

--- redux/features/postsSlicer.js
+++ redux/features/postsSlicer.js
@@ -6,7 +6,7 @@ const initialState = {
       author: { id: '434894384239242432', name: 'Natasha' },
       publishedDate: '2017-12-21',
       comment:
-         'Just completed my first training session, absolutely EXHAUSTED. Thanks so much everyone for your support.',
+         'Just completed my first training session, absolutely EXHAUSTED. Thanks so much everyone for your support. \n Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut risus faucibus, ornare lorem nec, ullamcorper arcu. Etiam faucibus massa at mattis ornare.Proin mattis purus quis magna commodo rhoncus at sit amet lectus. Integer arcu justo, pellentesque in eros cursus, porttitor vehicula magna.',
       comments: [
          {
             id: '3242342342j432lk',
@@ -15,7 +15,6 @@ const initialState = {
          },
       ],
       impressions: { loves: 1, stars: 3, likes: 45, hot: 43 },
-     
    },
 }
 
