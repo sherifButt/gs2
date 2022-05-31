6a5cd97db1e9328252fbde5584d218939d057f83
sherif.butt@gmail.com <sherif.butt@gmail.com>
2022-05-31 08:56:02 +0100

    GS-88  Built User drop down menu for logged in top menu

--- components/avatar/AvatarUser.jsx
+++ components/avatar/AvatarUser.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-const AvatarUser = ( { className, image } ) => {
-    const user={};
-    if(!image)  image = useSelector(state => state.user.image)
-  return (
-     <img
-        className={`block rounded-full w-12 border-2 border-white ${className}`}
-        src={image}
-     />
-  )
+const AvatarUser = ({ className, image }) => {
+   const user = useSelector(state => state.user)
+   if (!image) image = user.image
+   return (
+      <img
+         className={`block rounded-full w-12 border-2 border-white ${className}`}
+         src={image}
+      />
+   )
 }
 
 export default AvatarUser
