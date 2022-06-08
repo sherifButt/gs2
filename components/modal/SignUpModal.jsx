305dcc1635eeeb09c06801d435d889992c2e6d34
sherif.butt@gmail.com <sherif.butt@gmail.com>
2022-06-08 10:14:08 +0100

    GS2-85 adjusted signup modal to mobile sliding duration to 500ms

--- components/modal/SignUpModal.jsx
+++ components/modal/SignUpModal.jsx
@@ -127,8 +127,8 @@ export default function SignUpModal() {
                   className='flex items-end sm:items-center justify-center min-h-full  text-center'>
                   <Transition.Child
                      as={Fragment}
-                     enter='ease-out duration-300'
-                     enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+                     enter='ease-out duration-500'
+                     enterFrom='opacity-100 sm:opacity-0 translate-y-96 sm:translate-y-0 sm:scale-95'
                      enterTo='opacity-100 translate-y-0 sm:scale-100'
                      leave='ease-in duration-200'
                      leaveFrom='opacity-100 translate-y-0 sm:scale-100'
