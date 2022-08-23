4bb379f3ab1e3d63868035c49540e291f28d1666
sherifButt <90522472+sherifButt@users.noreply.github.com>
2022-08-23 15:52:21 +0100

    Update ButtonPrimary.jsx

--- components/buttons/ButtonPrimary.jsx
+++ components/buttons/ButtonPrimary.jsx
@@ -29,7 +29,7 @@ const ButtonPrimary = ({
             } rounded-xl ease-in-out duration-300 active:shadow-none text-center  font-semibold uppercase ${_className}`}>
             {isLoading ? (
                <div className='inline-flex'>
-                   {/*  
+                    
                          <svg
                      className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
                      xmlns='http://www.w3.org/2000/svg'
@@ -49,7 +49,7 @@ const ButtonPrimary = ({
                         d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
                   </svg>
                         
-                        */}
+                       
                  
                   <p>{action}</p>
                </div>
